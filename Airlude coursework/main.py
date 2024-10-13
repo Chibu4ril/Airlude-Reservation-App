e670bc92e39104319f93131403edc30b3cdac761
chibu4ril <successskido@gmail.com>
2024-10-13 22:31:11 +0100

    update

--- Airlude coursework/main.py
+++ Airlude coursework/main.py
@@ -1,13 +1,14 @@
-from tkinter import *
-from tkinter import ttk
+# from tkinter import *
+# from tkinter import ttk
+#
+# root = Tk()
+# frame = ttk.Frame(root, padding=10)
+#
+# frame.grid()
+#
+# ttk.Label(frame, text="Hello World!").grid(column=0, row=0)
+#
+# ttk.Button(frame, text="Quit", command=root.destroy).grid(column=0, row=1)
+#
+# root.mainloop()
 
-root = Tk()
-frame = ttk.Frame(root, padding=10)
-
-frame.grid()
-
-ttk.Label(frame, text="Hello World!").grid(column=0, row=0)
-
-ttk.Button(frame, text="Quit", command=root.destroy).grid(column=0, row=1)
-
-root.mainloop()
