13643a38cc4e2198e980e7896d152c51c4a1dc05
Innocent <successskido@gmail.com>
2024-10-22 10:09:29 +0100

    ticket counter

---
+++ Airlude coursework/starter.py
@@ -0,0 +1,19 @@
+class Tickets:
+    def __init__(self):
+        self.totalTicket = 100
+        self.ticketBooked = self.totalTicket
+
+    def bookedTicket(self, booking):
+        self.totalTicket -= booking
+
+
+
+
+
+request = Tickets()
+
+
+#
+#
+# if __init__ = '__main__':
+#
