1c68f02163514975bdea974c6a3338cbf2c68d1d
Innocent <successskido@gmail.com>
2024-10-22 13:54:00 +0100

    class creation

--- Airlude coursework/sandbox.py
+++ Airlude coursework/sandbox.py
@@ -1,19 +1,35 @@
 import random
 
-count = 0
-def customer_id():
-    c_id_range = range(0, 3)
-    customerId = []
+class CustomerBookingID:
+    def __init__(self):
+        self.count = 0
+        self.booking_digit_count = 0
+
+        def customer_id():
+            c_id_range = range(0, 3)
+            customerId = []
+            while self.count <= 3:
+                for i in c_id_range:
+                    random_range = random.randrange(5)
+                    customerId.append(i + random_range)
+                break
+            random_num_gen = ''.join(str(item) for item in customerId )
+            return random_num_gen
+
+        def booking_id():
+            c_id_range = range(0, 5)
+            bookingId = []
+            while self.booking_digit_count <= 5:
+                for i in c_id_range:
+                    random_range = random.randrange(5)
+                    bookingId.append(i + random_range)
+                break
+            random_booking_gen = ''.join(str(item) for item in bookingId)
+            return random_booking_gen
+
+ticketID = CustomerBookingID()
 
 
-    while count <= 3:
-        for i in c_id_range:
-            random_range = random.randrange(5)
-            customerId.append(i + random_range)
-        break
-    uniq = ''.join(str(item) for item in customerId )
-    return uniq
 
-print(customer_id())
 
 
