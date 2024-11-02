4e16cb6bc2b358c5eb552c499e8cb728950707bd
chibu4ril <successskido@gmail.com>
2024-11-02 16:09:00 +0000

    update

--- Airlude coursework/welcome.py
+++ Airlude coursework/welcome.py
@@ -1,12 +1,21 @@
 from tkinter import *
 from tkinter import ttk
+#
 root = Tk()
-l =ttk.Label(root, text="Starting...")
-l.grid()
-l.bind('<Enter>', lambda e: l.configure(text='Moved mouse inside'))
-l.bind('<Leave>', lambda e: l.configure(text='Moved mouse outside'))
-l.bind('<ButtonPress-1>', lambda e: l.configure(text='Clicked left mouse button'))
-l.bind('<3>', lambda e: l.configure(text='Clicked right mouse button'))
-l.bind('<Double-1>', lambda e: l.configure(text='Double clicked'))
-l.bind('<B3-Motion>', lambda e: l.configure(text='right button drag to %d,%d' % (e.x, e.y)))
+content = ttk.Frame(root, padding=(3, 3, 12, 12))
+
+frame = ttk.Frame(content, width=500, height=400 )
+
+content.grid(column=0, row=0, sticky=(N, S, E, W))
+frame.grid(column=0, row=0, columnspan=2, rowspan=2, sticky=(N, S, E, W))
+bookTicket = ttk.Button(content, text='Book A Ticket').grid(column=3, row=2, columnspan=4)
+
+
+updateTicket = ttk.Button(content, text='Book A Ticket').grid(column=6, row=0)
+cancelTicket = ttk.Button(content, text='Book A Ticket').grid(column=6, row=1)
+viewTickets = ttk.Button(content, text='Book A Ticket').grid(column=6, row=3)
+
+
+quit = ttk.Button(content, text='Quit', command=root.destroy).grid(column=3, row=3)
+
 root.mainloop()
